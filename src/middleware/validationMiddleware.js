f3431805000cacdac61d618621bf05573201cab4
Evan Natale <enatale1@ycp.edu>
2026-03-20 01:28:56 -0400

    BUGFIX: phoneNumber and levelOfStudy on the registration page no longer throw errors for characters/symbols that are valid. validationMiddleware was updated to fix this.

--- src/middleware/validationMiddleware.js
+++ src/middleware/validationMiddleware.js
@@ -3,21 +3,37 @@ const checkBodyForSpecialCharacters = (req, res, next) => {
     const ignoreFields = ["imageUrl", "password"]
 
     // Blocks characters often used in attacks ($, %, #, <, >, etc.)
-    const generalRegex = /[^a-zA-Z0-9\s-',\.:/\?&_=()@]/g
+    const generalRegex = /[^a-zA-Z0-9\s-',\.:/\?&_=()@]/;
 
     // Blocks characters for phoneNumber (allows only digits, spaces, plus, parens, and hyphens)
-    const phoneRegex = /[^0-9\s\+\-\(\)]/g
+    const phoneRegex = /[^0-9\s\+\-\(\)]/;
+
+    // Blocks characters for Level of Study
+    const studyRegex = /[^a-zA-Z0-9\s\/\(\)\-\+\',’]/;
 
     for (const key in req.body) {
         if (ignoreFields.includes(key)) continue; // Skip validation for fields we don't want to check
         const value = req.body[key];
+
+        // Skip non-string values
+       // if (typeof value !== 'string') continue;
 
         if(key === 'phoneNumber'){
             if(phoneRegex.test(value)){
                 // Will return 400 if restricted character is found
                 return res.status(400).json({
                     error: "Invalid Phone Number",
-                    message: "Phone numbers can only contain numbers, spaces, and symbols like +, -, and ( )."
+                    message: "Phone numbers should only include numbers and symbols like +, -, or ( )."
+                });
+            }
+        }
+
+        else if(key === 'levelOfStudy'){
+            if(studyRegex.test(value)){
+                // Will return 400 if restricted character for levelOfStudy is found
+                return res.status(400).json({
+                    error: "Invalid Level of Study",
+                    message: "that aint right"
                 });
             }
         }
